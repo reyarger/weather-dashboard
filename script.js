0bc38fe8a03471b641bf05784c76b9e3cb531f21
Rachel Yarger <ryarger@deposco.com>
2020-10-14 23:36:26 -0400

    began adding function to retain city name in webpage

--- script.js
+++ script.js
@@ -88,7 +88,48 @@
           $(".humid4").text("Humidity: "+week.list[28].main.humidity+" %");
           $(".humid5").text("Humidity: "+week.list[36].main.humidity+" %");
 
+          // function renderButtons() {
 
+          //   var searches=[];
+
+            // Deleting the movie buttons prior to adding new movie buttons
+            // (this is necessary otherwise we will have repeat buttons)
+            // $("#search-list").empty();
+    
+            // Looping through the array of searches
+            // for (var i = 0; i < searches.length; i++) {
+    
+              // Then dynamicaly generating buttons for each city search in the array.
+              // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
+              // var a = $("<button>");
+              // Adding a class
+              // a.addClass("city-search");
+              // Adding a data-attribute with a value of the city at index i
+              // a.attr("data-name", searches[i]);
+              // Providing the button's text with a value of the movie at index i
+              // a.text(searches[i]);
+              // Adding the button to the HTML
+          //     $("#search-list").append(a);
+          //   }
+          // }
+    
+          // This function handles events where one button is clicked
+          // $("#search-btn").on("click", function(event) {
+            // event.preventDefault() prevents the form from trying to submit itself.
+            // We're using a form so that the user can hit enter instead of clicking the button if they want
+            // event.preventDefault();
+    
+            // This line will grab the text from the input box
+            // var newCity = $("#search-input").val().trim();
+            // The movie from the textbox is then added to our array
+            // searches.push(newCity);
+    
+            // calling renderButtons which handles the processing of city array
+          //   renderButtons();
+          // });
+    
+          // Calling the renderButtons function at least once to display the initial list of movies
+          // renderButtons();
 
 
         });
